7b99c90baaaf6ec4e34ac473e4798a509239e9ec
Chris Close <christopher.close@gmail.com>
2013-11-23 22:02:32 +1100

    Make default build file

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,5 +3,5 @@
 export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.4.3/bin/arm-eabi-
 
-make msm8660-perf_P5LTE_EUR_OPEN_defconfig
+make msm8660-perf_P5LTE_EUR_OPEN_CL0SeY_defconfig
 make
